d1aaf966a180ac9d0a35ec70a63aaba848f8b38d
Julia <julialn.sunni@gmail.com>
2020-09-26 02:46:28 -0400

    socket.io

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
 
-app.use(express.static)
+app.use(express.static(`${__dirname}/../client`));
+
+const server = http.createServer(app);
+
+server.on("error", (err) => {
+  console.log(err);
+});
+
+server.listen(8080, () => {
+  console.log('server is ready');
+});
